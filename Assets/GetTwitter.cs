bc9bfe45e120ada680c32157fdbab377ce6e8040
Michael <michael@michaelhazani.com>
2017-02-25 16:23:04 -0800

    -get http working!

---
+++ Assets/GetTwitter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GetTwitter : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(GetText());
+    }
+
+    IEnumerator GetText()
+    {
+        using (UnityWebRequest request = UnityWebRequest.Get("http://localhost:3000/"))
+        {
+            yield return request.Send();
+
+            if (request.isError) // Error
+            {
+                Debug.Log(request.error);
+            }
+            else // Success
+            {
+                Debug.Log(request.downloadHandler.text);
+            }
+        }
+    }
+}
